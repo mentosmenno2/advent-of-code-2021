82c3d3d9e8a63833bc5475a6f5ceb0ebfe4af6ea
mentosmenno2 <mentosmenno2@hotmail.com>
2021-12-19 03:01:38 +0100

    add day 5 part 2

---
+++ 05/2/Coordinate.php
@@ -0,0 +1,25 @@
+<?php
+
+class Coordinate
+{
+
+    /** @var int */
+    protected $x;
+
+    /** @var int */
+    protected $y;
+
+    public function __construct( int $x, int $y )
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+
+    public function get_x(): int {
+        return $this->x;
+    }
+
+    public function get_y(): int {
+        return $this->y;
+    }
+}
