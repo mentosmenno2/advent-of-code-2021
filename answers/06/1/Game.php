778955ac4629c177ac8a021f3895ac1e3e64f549
mentosmenno2 <mentosmenno2@hotmail.com>
2021-12-19 20:13:05 +0100

    add day 6 part 1

---
+++ answers/06/1/Game.php
@@ -0,0 +1,53 @@
+<?php
+
+class Game
+{
+
+	/** @var LanternFish[] */
+	protected $fish = array();
+
+	protected $days_to_simulate;
+
+	public function __construct(string $data_file, int $days_to_simulate = 80)
+	{
+		$this->load_game_data($data_file);
+		$this->days_to_simulate = $days_to_simulate;
+		$this->start_game();
+	}
+
+	protected function load_game_data(string $data_file): void
+	{
+		$file_content = file_get_contents($data_file);
+		$file_data    = explode(',', $file_content);
+		$this->fish  = array_map(
+			function (string $internal_fish_timer): LanternFish {
+				return new LanternFish((int) $internal_fish_timer);
+			},
+			$file_data
+		);
+	}
+
+	protected function start_game(): void
+	{
+		$fish_timers = array_map(function (LanternFish $fish): int {
+			return $fish->get_internal_timer();
+		}, $this->fish);
+		// echo sprintf('Initial state: %s <br>', implode(',', $fish_timers));
+
+		for ($day=1; $day < $this->days_to_simulate + 1; $day++) {
+			foreach ($this->fish as $fish) {
+				if ($fish->can_give_birth()) {
+					$this->fish[] = new LanternFish();
+				}
+				$fish->pass_internal_timer_day();
+			}
+
+			$fish_timers = array_map(function (LanternFish $fish): int {
+				return $fish->get_internal_timer();
+			}, $this->fish);
+			// echo sprintf('After %d days: %s <br>', $day, implode(',', $fish_timers));
+		}
+
+		echo sprintf('<strong>After %d days the total amount of fish is: %s </strong>', $this->days_to_simulate, count($this->fish));
+	}
+}
---
+++ answers/06/1/index.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once 'LanternFish.php';
+require_once 'Game.php';
+
+$game = new Game(__DIR__ . '/../data.txt');
