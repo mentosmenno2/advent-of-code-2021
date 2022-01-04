d8989cce1d0731eb8b4dc1fb49b93cf43a78d9c2
mentosmenno2 <mentosmenno2@hotmail.com>
2022-01-04 23:21:08 +0100

    day 8 part 1

--- answers/08/1/Game.php
+++ answers/08/1/Game.php
@@ -3,6 +3,8 @@
 class Game
 {
 
+	protected $entries = array();
+
 	public function __construct(string $data_file)
 	{
 		$this->load_game_data($data_file);
@@ -12,10 +14,28 @@ class Game
 	protected function load_game_data(string $data_file): void
 	{
 		$file_content = file_get_contents($data_file);
+		$file_content = explode(PHP_EOL, $file_content);
+		$file_content = array_map(function (string $entry): array {
+			$entry_parts = explode(' | ', $entry);
+			return array(
+				'signal_patterns' => array_map('str_split', explode(' ', $entry_parts[0])),
+				'output_value' => array_map('str_split', explode(' ', $entry_parts[1])),
+			);
+		}, $file_content);
+		$this->entries = $file_content;
 	}
 
 	protected function start_game(): void
 	{
-		
+		$count = 0;
+		foreach ($this->entries as $entry) {
+			foreach ($entry['output_value'] as $output_value) {
+				if (in_array(count($output_value), array( 2, 4, 3, 7 ))) {
+					$count++;
+				}
+			}
+		}
+
+		echo $count;
 	}
 }
